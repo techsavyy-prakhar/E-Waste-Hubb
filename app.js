c39b541913c05b357bd73e5fd393f6a1e2df8304
“techsavyy <prakhar.aggarwal.ug20@nsut.ac.in>
2023-09-20 20:27:03 +0530

    20.26

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 const path = require("path");
 const mongoose = require("mongoose");
